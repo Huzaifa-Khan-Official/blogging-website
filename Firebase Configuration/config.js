5405924848bf94d6457d5d90e037120199b86560
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-01-19 23:24:44 +0500

    authentication almost done

--- Firebase Configuration/config.js
+++ Firebase Configuration/config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signOut  } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore, collection, addDoc, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -18,4 +18,4 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 
-export {app, auth, createUserWithEmailAndPassword, db, collection, addDoc, doc, setDoc }
+export {app, auth, createUserWithEmailAndPassword, db, collection, addDoc, doc, setDoc, onAuthStateChanged, signOut }
